e68b3404121ac2f758093d948cd44b3023235dba
elwillan <wgarnicaw@gmail.com>
2023-08-01 01:21:19 +0000

    configuration junit 5

---
+++ src/main/java/exercises/Exercises.java
@@ -0,0 +1,7 @@
+package exercises;
+
+public class Exercises {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+    }
+}
---
+++ src/test/java/exercises/validateformatstring1/ValidateFormatString1Test.java
@@ -0,0 +1,27 @@
+package exercises.validateformatstring1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ValidateFormatString1Test {
+
+    @Test
+    public void isValid() {
+        ValidateFormatString1 validateFormatString = new ValidateFormatString1();
+        assertTrue( validateFormatString.isValid(""));
+        assertTrue( validateFormatString.isValid("()"));
+        assertTrue( validateFormatString.isValid("[()]"));
+        assertTrue( validateFormatString.isValid("{()}"));
+        assertTrue( validateFormatString.isValid("[]{}()"));
+        assertTrue( validateFormatString.isValid("[{()}]"));
+        assertTrue( validateFormatString.isValid("[{{{[[([()])]]}}}]{()}"));
+
+        assertFalse( validateFormatString.isValid(null));
+        assertFalse( validateFormatString.isValid(")"));
+        assertFalse( validateFormatString.isValid("())"));
+        assertFalse( validateFormatString.isValid("{(})"));
+        assertFalse( validateFormatString.isValid("{([)})]"));
+    }
+}
