b67b88fca6b013fe8cea19b9200868c32cc28a93
elwillan <williamgarnica@ingenieros.com>
2023-07-31 19:18:18 -0500

    migration to junit 5

--- src/main/java/exercises/validateformatstring1/ValidateFormatString1Test.java
+++ src/main/java/exercises/validateformatstring1/ValidateFormatString1Test.java
@@ -1,9 +1,10 @@
 package exercises.validateformatstring1;
 
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.Assert.*;
 
 public class ValidateFormatString1Test {
 
